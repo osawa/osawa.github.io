f86d7d4b1de8fd1d9f97e4ce11b713ba7f17e68c
Yuki Osawa <yuki.osawa@gmail.com>
2021-08-09 15:16:37 +0900

    tailwind(twin.macro)導入

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -1,8 +1,14 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+
+// tailwind
+import "tailwindcss/dist/base.css"
+// import "tailwindcss/dist/components.css";
+// import "tailwindcss/dist/utilities.css";
+
 // normalize CSS across browsers
-import "./src/normalize.css"
+// import "./src/normalize.css"
 // custom CSS styles
 import "./src/style.css"
 
