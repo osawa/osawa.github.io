913b01dd7a96a5fa5535d104b58fd2200cfdd42c
Yuki Osawa <yuki.osawa@gmail.com>
2021-08-17 22:35:26 +0900

    v0.2 詠唱値ベースと出目ベースで表を分割

--- gatsby/src/pages/spell-odds.js
+++ gatsby/src/pages/spell-odds.js
@@ -8,21 +8,22 @@ import Seo from "../components/seo"
 import Tags from "../components/tags"
 
 const SpellOdds = ({ data, location }) => {
+  const version = '0.2';
   const siteTitle = data.site.siteMetadata?.title || `Title`
 
-  const [castValue, scb] = useState(6);
-  const setCastValue = (e) => scb(e.target.value);
+  const [castValue, setCastValue] = useState(12);
+  const [rollValue, setRollValue] = useState(6);
 
-  const getOdds = (value = castValue) => {
+  const getRollOdds = (value) => {
     return (0 - Math.abs(value - 7) + 6) / 36;
   };
 
   const getCastOdds = (value = castValue) => {
-    let v = value == 2 ? 3 : value; // 出目2はファンブルなので詠唱値3と同値扱い
+    let v = value == 2 ? 3 : parseInt(value); // 出目2はファンブルなので詠唱値3と同値扱い
     let odds = 0;
     return (() => {
       for (let i = v; i < 13; i++) {
-        odds += getOdds(i);
+        odds += getRollOdds(i);
       }
       return odds;
     })();
@@ -30,8 +31,7 @@ const SpellOdds = ({ data, location }) => {
 
   const getUnbindOdds  = (value = castValue) => {
     if (value >= 12) return 0;
-    const v = value == 2 ? 3 : value;
-    return getCastOdds(parseInt(v) + 1);
+    return getCastOdds(parseInt(value) + 1);
   };
 
   const getNotUnbindOdds = value => {
@@ -43,11 +43,8 @@ const SpellOdds = ({ data, location }) => {
     let odds = 0;
     return (() => {
       for (let i = v; i < 13; i++) {
-        const so = getOdds(i) * getNotUnbindOdds(i);
+        const so = getRollOdds(i) * getNotUnbindOdds(i);
         odds += so;
-        console.log('getOdds: ' + getOdds(i));
-        console.log('getNotUnbindOdds: ' + getNotUnbindOdds(i));
-        console.log('so: ' + so);
       }
       return odds;
     })();
@@ -59,38 +56,53 @@ const SpellOdds = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <div css={tw`flex items-center justify-between`}>
-        <h1 css={tw`m-0`}>呪文確率計算機</h1>
-        <small>ver 0.1</small>
+      <div css={tw`flex flex-col space-y-8`}>
+        <div css={tw`flex items-center justify-between`}>
+          <h1 css={tw`m-0`}>WH:AoS 呪文確率計算機</h1>
+          <small>Ver {version}</small>
+        </div>
+        <div css={tw`flex flex-col md:flex-row md:space-x-4`}>
+          <div css={tw`flex-1`}>
+            <h2>詠唱値ベースでの成功率</h2>
+            <div css={tw`flex space-x-4 items-center`}>
+              <div>詠唱値</div>
+              <input type="number" min={2} max={12} onChange={(e) => setCastValue(e.target.value)} css={inputStyles} value={castValue} />
+            </div>
+            <table css={tw`border mt-4`}>
+              <tr>
+                <th css={thStyles}>詠唱成功率</th>
+                <td css={tdStyles}>{toPercent(getCastOdds(castValue))}</td>
+              </tr>
+              <tr>
+                <th css={thStyles}>詠唱成功＆打ち消し失敗率</th>
+                <td css={tdStyles}>{toPercent(getFinallySuccessOdds(castValue))}</td>
+              </tr>
+            </table>
+          </div>
+          <div css={tw`flex-1`}>
+            <h2>詠唱出目に対する打ち消し成功率</h2>
+            <div css={tw`flex space-x-4 items-center`}>
+              <div>詠唱出目</div>
+              <input type="number" min={3} max={12} onChange={(e) => setRollValue(e.target.value)} css={inputStyles} value={rollValue} />
+              <small>※2はファンブルなので3-12</small>
+            </div>
+            <table css={tw`border mt-4`}>
+              <tr>
+                <th css={thStyles}>打ち消し成功値</th>
+                <td css={tdStyles}>{parseInt(rollValue)+ 1}</td>
+              </tr>
+              <tr>
+                <th css={thStyles}>打ち消し成功率</th>
+                <td css={tdStyles}>{toPercent(getUnbindOdds(rollValue))}</td>
+              </tr>
+              <tr>
+                <th css={thStyles}>打ち消し失敗率</th>
+                <td css={tdStyles}>{toPercent(getNotUnbindOdds(rollValue))}</td>
+              </tr>
+            </table>
+          </div>
+        </div>
       </div>
-      <div css={tw`flex space-x-4 items-center mt-8`}>
-        <div>詠唱値</div>
-        <input type="number" min={2} max={12} name="" id="" onChange={setCastValue} css={inputStyles} value={castValue} />
-      </div>
-      <table css={tw`border mt-4`}>
-        <tr>
-          <th css={thStyles}>詠唱値</th>
-          <td css={tdStyles}>{castValue}</td>
-          <th css={thStyles}>打ち消し成功値</th>
-          <td css={tdStyles}>{parseInt(castValue)+ 1}</td>
-        </tr>
-        <tr>
-          <th css={thStyles}>詠唱成功率</th>
-          <td css={tdStyles}>{toPercent(getCastOdds(castValue))}</td>
-          <th css={thStyles}>打ち消し成功率</th>
-          <td css={tdStyles}>{toPercent(getUnbindOdds(castValue))}</td>
-        </tr>
-        <tr>
-          <th css={thStyles}>詠唱成功＆打ち消し失敗率</th>
-          <td css={tdStyles}>{toPercent(getFinallySuccessOdds(castValue))}</td>
-          <th css={thStyles}>打ち消し失敗率</th>
-          <td css={tdStyles}>{toPercent(getNotUnbindOdds(castValue))}</td>
-        </tr>
-      </table>
-      {/* <p>Odds: {getOdds()}</p>
-      <p>UnbindOdds: {getCastOdds(castValue + 1)}</p>
-      <p>NotUnbindOdds: {getNotUnbindOdds(castValue)}</p>
-      <p>FinallySuccessOdds: {toPercent(getFinallySuccessOdds(castValue))}</p> */}
     </Layout>
   )
 }
